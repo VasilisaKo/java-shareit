d1ebb526fc0dc7b4f472098404b4dd172385aded
Vasilisa <vasjatinka@yandex.ru>
2023-06-23 01:28:59 +0100

    feat: add test class

---
+++ src/test/java/ru/practicum/shareit/jsondto/JsonItemDtoTest.java
@@ -0,0 +1,45 @@
+package ru.practicum.shareit.jsondto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import ru.practicum.shareit.item.CommentMapper;
+import ru.practicum.shareit.item.ItemMapper;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.CommentResponseDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class JsonItemDtoTest {
+    @Autowired
+    private JacksonTester<Item> jsonItem;
+    @Autowired
+    private JacksonTester<ItemDto> jsonItemDto;
+
+    @Autowired
+    private JacksonTester<CommentResponseDto> jsonCommentResponseDto;
+
+    @Test
+    void testItemDto() throws Exception {
+        User user = new User(1, "Test User", "test@example.com");
+        Item item = new Item(1, "Test Item", "Test Description", user, true, 1);
+        ItemDto itemDto = ItemMapper.toItemDto(item);
+
+        ItemDto itemDtoNew = new ItemDto(2, "Test ItemDto", "Test DescriptionDto", user, true, 1);
+        Item newItem = ItemMapper.toItem(itemDtoNew, new User());
+
+        JsonContent<Item> result = jsonItem.write(newItem);
+
+        assertThat(result).extractingJsonPathStringValue("$.name").isEqualTo("Test ItemDto");
+        assertThat(result).extractingJsonPathStringValue("$.description").isEqualTo("Test DescriptionDto");
+    }
+}
